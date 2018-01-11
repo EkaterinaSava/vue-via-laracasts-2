48235698559574dc3e4e9480c26f48e26bea297c
EkaterinaSava <ek@weare.lt>
2018-01-11 17:55:20 +0300

    ADD demo for video #16: ADD new component "Message" with export for "App"

---
+++ src/components/Message.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="box">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style>
+  .box {
+    width: 200px;
+    background-color: lightblue;
+    border: 1px solid gray;
+    padding: 10px;
+    margin: 20px auto;
+  }
+</style>
